baf162270671e387700c7170da87e03eeeb7fde3
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-09-26 16:12:51 +0100

    variable types and installed mypy

--- app.py
+++ app.py
@@ -1,19 +1,3 @@
-students_count = 1000
-rating = 4.99
-is_published = True
-course_name = "Python"
-second_course_name = 'Thryton'
-# triple quotations when multiple lines
-third_course_name = """
-Mulitple
-Lines 
-"""
-
-x = 1
-y = 2
-# same as above
-x, y = 1, 2
-
-# set mulitple variables to the same value
-
-x = y = 1
+age: int = 20
+age = "Python"
+print(age)
