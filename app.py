383132ada8413c112d3f9dfcc2e7db88e09ad683
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-09-26 16:30:02 +0100

    strings, slicing and immutability

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-x = [1, 2, 3]
-print(id(x))
+course = 'Python Programming'
+print(len(course))
+print(course[2])
+# to print the last character of the string
+print(course[-1])
+# to slice, this will return the first three characters in the string
+print(course[0:3])
+# this will return the same as above
+print(course[:3])
+# will return the entire string
+print(course[0:])
+# will also return the entire string
+print(course[:])
 
-x.append(4)
-print(id(x))
+# strings are immutable in Python, hence this results in different memory locations when run
+print(id(course))
+print(id(course[0]))
