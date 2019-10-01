c30aa0ffa1fd7183d679b93c7f34e0841bd64dc5
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-10-01 14:06:55 +0100

    using escape character in python

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-course = 'Python Programming'
-print(len(course))
-print(course[2])
-# to print the last character of the string
-print(course[-1])
-# to slice, this will return the first three characters in the string
-print(course[0:3])
-# this will return the same as above
-print(course[:3])
-# will return the entire string
-print(course[0:])
-# will also return the entire string
-print(course[:])
 
-# strings are immutable in Python, hence this results in different memory locations when run
-print(id(course))
-print(id(course[0]))
+# using escape character (backslash) to interpret a character differently
+message = "Python \"Programming"
+print(message)
+
+# \"
+# \'
+# \\
+# \n - for new line like below
+
+mess = "Python \nProgramming"
+print(mess)
+
+# but if writing many lines of code, better to use triple quotes, like so
+example = """
+Python 
+Programming
+"""
+print(example)
