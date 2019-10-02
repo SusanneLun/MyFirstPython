00488ffc836a5680fc8cccf4e611155ef2e7d4b5
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2019-10-02 10:24:16 +0100

    arithmetic operators

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-x = 10
-x = 0b10
-print(x)
-# takes any number and returns its binary representation
-print(bin(x))
-# numbers in hexadecimal format
-x = 0x12c
-print(hex(x))
+x = 10 + 3
+x = 10 - 3
+x = 10 * 3
+# will give you decimals
+x = 10 / 3
+# will give you an integer
+x = 10 // 3
+# remainder of a division
+x = 10 % 3
+# exponent
+x = 10 ** 3
 
-# a + bi
-# j is like i in maths, you can use upper or lower case
-x = 1 + 2j
+# if we want to add one to x, we can either
+x = x + 1
+# OR we can use an augmented operator
+x = + 1
+
 print(x)
